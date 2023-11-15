564ea2f4a1fce6045268a0118986e0aa720bc3df
Spenser Hill <87867697+Spenserhill64@users.noreply.github.com>
2023-11-15 17:22:35 -0500

    Completed Loop & Logic

--- Guessing_Game.py
+++ Guessing_Game.py
@@ -2,5 +2,24 @@
 
 import random
 
+print("I'm thinking of a number between one and twenty. Can you guess the number?")
+
 random_integer = random.randint(1, 20)
-print(random_integer)
+
+while True:
+    number = input('Please enter your best guess here: ')
+    
+    try:
+        number = int(number)
+    except ValueError:
+        print("Please enter a valid integer.")
+        continue  # Skip the rest of the loop and start a new iteration
+
+    if 1 <= number <= 20:
+        if number == random_integer:
+            print("Well look at you, that is the right number.")
+            break  # Exit the loop if the correct number is guessed
+        else:
+            print("Try again!")
+    else:
+        print("Please enter a number between 1 and 20.")
