9f5d0772694282d0fc86797e96670773af80f176
Spenser Hill <87867697+Spenserhill64@users.noreply.github.com>
2024-01-04 16:25:56 -0500

    Update IF_Train.py

--- IF_Train.py
+++ IF_Train.py
@@ -1,8 +1,9 @@
 # write a program that takes an age and will tell you if you are able to drive; assuming a legal driving age is 16.
-
+# Establishing variables
 age = input('Please enter your age to continue...>>')
 age = int(age)
 
+# if statement
 if age > 16:
     print("Here are the carkeys")
 elif age == 16:
